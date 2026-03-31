036c84f9c1715a20676a89474b8008385ba7516f
grigoriev <alexandre.grigoriev@horiba.com>
2026-03-31 10:51:37 +0200

    auth design

--- frontend/src/components/auth/AuthDialog.tsx
+++ frontend/src/components/auth/AuthDialog.tsx
@@ -99,7 +99,7 @@ export function AuthDialog({ open, onBeginOAuth, onSuccess, onClose }: {
   );
 
   const WindowsBtn = () => (
-    <button className="authProviderBtn" type="button" onClick={() => { setScreen("ldap"); setError(""); setPassword(""); }}>
+    <button className="authProviderBtn" type="button" style={{ marginBottom: 20 }} onClick={() => { setScreen("ldap"); setError(""); setPassword(""); }}>
       <svg viewBox="0 0 24 24" width="20" height="20">
         <path fill="#00adef" d="M0 3.449L9.75 2.1v9.451H0m10.949-9.602L24 0v11.551H10.949M0 12.6h9.75v9.451L0 20.699M10.949 12.6H24V24l-12.9-1.801"/>
       </svg>
